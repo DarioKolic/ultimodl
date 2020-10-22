b892292ed5901a9752e4d4a9947c91bb16b54870
Dario Kolic <coachbuilds@gmail.com>
2020-10-22 21:35:17 +0200

    Updates

---
+++ precache-manifest.205e7fa5c022360d0b4709cf2b0749bc.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be21a4ea24e42285ca0cbffb7c1dc007",
+    "url": "/ultimodl/index.html"
+  },
+  {
+    "revision": "cf2eeded9c8ec4323e25",
+    "url": "/ultimodl/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "7ca574637e2fe7833337",
+    "url": "/ultimodl/static/css/main.904d42d7.chunk.css"
+  },
+  {
+    "revision": "cf2eeded9c8ec4323e25",
+    "url": "/ultimodl/static/js/2.38ee1681.chunk.js"
+  },
+  {
+    "revision": "c173fb49de882ba6a1987bc2dd59b7e1",
+    "url": "/ultimodl/static/js/2.38ee1681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca574637e2fe7833337",
+    "url": "/ultimodl/static/js/main.7173cfed.chunk.js"
+  },
+  {
+    "revision": "28ea11847a5db4607625",
+    "url": "/ultimodl/static/js/runtime-main.03434b96.js"
+  },
+  {
+    "revision": "5ad8014f564a728c4458aeeb2c583d87",
+    "url": "/ultimodl/static/media/1.5ad8014f.PNG"
+  },
+  {
+    "revision": "ba6150451beb1ac8e60cdba7f1d5e129",
+    "url": "/ultimodl/static/media/2.ba615045.png"
+  },
+  {
+    "revision": "32b9bffe0ca3ca99f315aa36da5efa17",
+    "url": "/ultimodl/static/media/3.32b9bffe.png"
+  },
+  {
+    "revision": "84afc7ded26b8ced57df2759ddf7fe9d",
+    "url": "/ultimodl/static/media/Group 8.84afc7de.svg"
+  },
+  {
+    "revision": "5920447fec6ae2fc95f796e01c1edef2",
+    "url": "/ultimodl/static/media/Logo_small.5920447f.png"
+  },
+  {
+    "revision": "944451d660568374770a0ba38b71bf1b",
+    "url": "/ultimodl/static/media/a.944451d6.png"
+  },
+  {
+    "revision": "54e5234bdfba82e33f12cdcf926a4544",
+    "url": "/ultimodl/static/media/b.54e5234b.png"
+  },
+  {
+    "revision": "6d43138f6124f40d3e47445e7dfef79c",
+    "url": "/ultimodl/static/media/background.6d43138f.png"
+  },
+  {
+    "revision": "c15ef8837e2578987d77042fb9f6dc44",
+    "url": "/ultimodl/static/media/background.c15ef883.mp4"
+  },
+  {
+    "revision": "47f806193bc7a35a2173a4244b5d0170",
+    "url": "/ultimodl/static/media/banner-overlay.47f80619.png"
+  },
+  {
+    "revision": "fe470e5582048635c411cb5523fc65f9",
+    "url": "/ultimodl/static/media/building.fe470e55.gif"
+  },
+  {
+    "revision": "e7f768f89b66e844ac3f188e5d08177d",
+    "url": "/ultimodl/static/media/c.e7f768f8.png"
+  },
+  {
+    "revision": "8299f58c70c9ae60a53075938be3fe95",
+    "url": "/ultimodl/static/media/d.8299f58c.png"
+  },
+  {
+    "revision": "e452d80f2a1f70271c4053f3b0e51ffc",
+    "url": "/ultimodl/static/media/dashboard.e452d80f.gif"
+  },
+  {
+    "revision": "587e67cbac7257f64bc97fbd18735aab",
+    "url": "/ultimodl/static/media/laptop.587e67cb.gif"
+  },
+  {
+    "revision": "bbc707b79d6f93a6c56e81745f7133ed",
+    "url": "/ultimodl/static/media/logo.bbc707b7.png"
+  },
+  {
+    "revision": "d093a23107d45cf2211884df7eb6660e",
+    "url": "/ultimodl/static/media/mobile.d093a231.gif"
+  },
+  {
+    "revision": "2de2890196ba34c2104e7d783adb09e2",
+    "url": "/ultimodl/static/media/safty.2de28901.gif"
+  },
+  {
+    "revision": "a6c3f4f448e00be74b365deb932b2918",
+    "url": "/ultimodl/static/media/temp.a6c3f4f4.png"
+  },
+  {
+    "revision": "a9c77ce9f5c03d2ccf348aefc12e3577",
+    "url": "/ultimodl/static/media/train.a9c77ce9.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6d8f929ff029c91ba453ba9d1bc2597.js"
+  "/ultimodl/precache-manifest.205e7fa5c022360d0b4709cf2b0749bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ultimodl/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
